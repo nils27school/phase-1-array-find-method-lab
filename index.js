9aaaf7fc011ee0d08cea157c576334b10b3358e2
nilssvensson <nils27school@gmail.com>
2022-09-02 11:48:42 -0400

    commit tree 9b06a194398972ea973a620cad21a9b3792bfdd4

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// code your solution here
+// // code your solution here
+// // const record = [
+// //     { year: "2015", result: "W"},
+// //     { year: "2014", result: "N/A"},
+// //     { year: "2013", result: "L"},
+// //     //...
+// //   ]
+  
+
+//   function superbowlWin(yearsAndResults) {
+// return yearsAndResults.find(callbackFn);
+//   }
+
+
+// function isWin(){
+//     if (some conditional) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// const obj = superbowlWin(record);
+// const obj = yearsAndResults.find(isWin)
+
+//   function isWin(gameData){
+//      was this a win or not
+//   if (gameData.result === "W") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+//  return obj.year;
+// }
+
+
+superbowlWin = (record) => {
+  const result = record.find( record => record.result === "W" );
+  return (result ? result.year : undefined);
+}
